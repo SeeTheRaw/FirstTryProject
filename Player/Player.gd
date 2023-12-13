9bb4d94557ef125db000d3a07e2b7054cab7d79a
Lorenzo Sittaro <124346464+SeeTheRaw@users.noreply.github.com>
2023-12-13 23:58:50 +0100

    Finally Adding functional Death Animation

--- Player/Player.gd
+++ Player/Player.gd
@@ -7,7 +7,6 @@ var JUMP_VELOCITY = -479.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
 @onready var anim = get_node("AnimationPlayer")
 		
 func _physics_process(delta):
@@ -18,12 +17,9 @@ func _physics_process(delta):
 	# Handle Jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-		if get_node("AnimatedSprite2D").animation != "Death":
-			anim.play("Jump")
-
+		anim.play("Jump")
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if HP == 1:
 		print(direction)
@@ -34,28 +30,22 @@ func _physics_process(delta):
 		if direction: 
 			velocity.x = direction * SPEED
 			if velocity.y == 0:
-				if get_node("AnimatedSprite2D").animation != "Death":
-					anim.play("Moving")
+				anim.play("Moving")
 		else:
 			velocity.x = move_toward(velocity.x, 0, SPEED)
 			if velocity.y == 0:
-				if get_node("AnimatedSprite2D").animation != "Death":
-					anim.play("Idle")
+				anim.play("Idle")
 		if velocity.y > 0:
-			if get_node("AnimatedSprite2D").animation != "Death":
-				anim.play("Fall")
+			anim.play("Fall") 
 	
 	move_and_slide()
 	
 	if HP <= 0:
-		get_node("AnimatedSprite2D").flip_h = false
 		velocity.x = 0
 		velocity.y = 0
-		SPEED = 0
 		JUMP_VELOCITY = 0
 		gravity = 0
-		direction = 0
-		get_node("AnimatedSprite2D").play("Death")
-		await get_node("AnimatedSprite2D").animation_finished
+		anim.play("Death")
+		await anim.animation_finished
 		queue_free()
 		get_tree().change_scene_to_file("res://main.tscn")
